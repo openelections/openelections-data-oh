a0fd14c7707aad9d879bec69632d31e1da79ddad
Christopher Jenness <christopherjenness@gmail.com>
2017-10-16 07:29:48 -0400

    Add districtless parsing

--- 2006/2006parser.py
+++ 2006/2006parser.py
@@ -34,31 +34,109 @@ REPUBLICAN_PRIMARY_URLS = {'Governor/LtGovernor': 'https://www.sos.state.oh.us/e
                            'State Representative': 'https://www.sos.state.oh.us/elections/election-results-and-data/2006-elections-results/republican-ohio-house-of-representatives-may-2-2006/'
                            }
 
-COLS = ['county', 'candidate', 'party', 'office', 'district', 'votes', 'pct']
+COLS = ['county', 'candidate', 'party', 'office', 'district', 'votes']
 
 
 def make_governor_df():
-    pass
+    df = pd.read_html(GENERAL_URLS['Governor/LtGovernor'])[0]
+    df.columns = df.iloc[0].str.rstrip(' (WI)').str.lstrip('*')
+    df['COUNTY'].iloc[1]='Party'
+    df = df.dropna(subset=['COUNTY'])
+    party_df = df.iloc[1]
+    party_df.columns = ['Candidate', 'Party']
+    df = df.iloc[2:]
+    df = (pd.melt(df, id_vars=['COUNTY'], value_vars=list(df.columns[1:])))
+    df.columns = ['county', 'candidate', 'votes']
+    df['office'] = 'GOVERNOR/LIEUTENANT GOVERNOR'
+    df['party'] = df.candidate.map(party_df)
+    df = df[~df.county.str.startswith('Percentage')]
+    df = df[~df.county.str.startswith('Total')]
+    return df
 
 
 def make_attorney_general_df():
-    pass
+    df = pd.read_html(GENERAL_URLS['Attorney General'])[0]
+    df.columns = df.iloc[3].str.rstrip(' (WI)').str.lstrip('*')
+    df['COUNTY'].iloc[4]='Party'
+    df = df.dropna(subset=['COUNTY'])
+    party_df = df.iloc[3]
+    party_df.columns = ['Candidate', 'Party']
+    df = df.iloc[5:]
+    df = (pd.melt(df, id_vars=['COUNTY'], value_vars=list(df.columns[1:])))
+    df.columns = ['county', 'candidate', 'votes']
+    df['office'] = 'ATTORNEY GENERAL'
+    df['party'] = df.candidate.map(party_df)
+    df = df[~df.county.str.startswith('Percentage')]
+    df = df[~df.county.str.startswith('Total')]
+    return df
 
 
 def make_state_auditor_df():
-    pass
+    df = pd.read_html(GENERAL_URLS['State Auditor'])[0]
+    df.columns = df.iloc[3].str.rstrip(' (WI)').str.lstrip('*')
+    df['COUNTY'].iloc[4]='Party'
+    df = df.dropna(subset=['COUNTY'])
+    party_df = df.iloc[3]
+    party_df.columns = ['Candidate', 'Party']
+    df = df.iloc[5:]
+    df = (pd.melt(df, id_vars=['COUNTY'], value_vars=list(df.columns[1:])))
+    df.columns = ['county', 'candidate', 'votes']
+    df['office'] = 'STATE AUDITOR'
+    df['party'] = df.candidate.map(party_df)
+    df = df[~df.county.str.startswith('Percentage')]
+    df = df[~df.county.str.startswith('Total')]
+    return df
 
 
 def make_secretary_of_state_df():
-    pass
+    df = pd.read_html(GENERAL_URLS['Secretary of State'])[0]
+    df.columns = df.iloc[3].str.rstrip(' (WI)').str.lstrip('*')
+    df['COUNTY'].iloc[4]='Party'
+    df = df.dropna(subset=['COUNTY'])
+    party_df = df.iloc[3]
+    party_df.columns = ['Candidate', 'Party']
+    df = df.iloc[5:]
+    df = (pd.melt(df, id_vars=['COUNTY'], value_vars=list(df.columns[1:])))
+    df.columns = ['county', 'candidate', 'votes']
+    df['office'] = 'SECRETARY OF STATE'
+    df['party'] = df.candidate.map(party_df)
+    df = df[~df.county.str.startswith('Percentage')]
+    df = df[~df.county.str.startswith('Total')]
+    return df
 
 
 def make_state_treasure_df():
-    pass
+    df = pd.read_html(GENERAL_URLS['State Treasure'])[0]
+    df.columns = df.iloc[3].str.rstrip(' (WI)').str.lstrip('*')
+    df['COUNTY'].iloc[4]='Party'
+    df = df.dropna(subset=['COUNTY'])
+    party_df = df.iloc[3]
+    party_df.columns = ['Candidate', 'Party']
+    df = df.iloc[5:]
+    df = (pd.melt(df, id_vars=['COUNTY'], value_vars=list(df.columns[1:])))
+    df.columns = ['county', 'candidate', 'votes']
+    df['office'] = 'STATE TREASURE'
+    df['party'] = df.candidate.map(party_df)
+    df = df[~df.county.str.startswith('Percentage')]
+    df = df[~df.county.str.startswith('Total')]
+    return df
 
 
 def make_us_senate_df():
-    pass
+    df = pd.read_html(GENERAL_URLS['US Senate'])[0]
+    df.columns = df.iloc[3].str.rstrip(' (WI)').str.lstrip('*')
+    df['COUNTY'].iloc[4]='Party'
+    df = df.dropna(subset=['COUNTY'])
+    party_df = df.iloc[3]
+    party_df.columns = ['Candidate', 'Party']
+    df = df.iloc[5:]
+    df = (pd.melt(df, id_vars=['COUNTY'], value_vars=list(df.columns[1:])))
+    df.columns = ['county', 'candidate', 'votes']
+    df['office'] = 'US SENATE'
+    df['party'] = df.candidate.map(party_df)
+    df = df[~df.county.str.startswith('Percentage')]
+    df = df[~df.county.str.startswith('Total')]
+    return df
 
 
 def make_us_house_of_reps_df():
@@ -66,4 +144,8 @@ def make_us_house_of_reps_df():
 
 
 def make_state_representatives_df():
+    pass
+
+
+def make_state_senate_df():
     pass
