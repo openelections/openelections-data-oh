efb1b8225867c5a9c66268ce9199291e87db76df
Derek Willis <dwillis@gmail.com>
2019-12-05 10:59:25 -0500

    merged

--- districts.py
+++ districts.py
@@ -1,7 +1,11 @@
 import json
+<<<<<<< Updated upstream
 import unicsv
+=======
+import csv
+>>>>>>> Stashed changes
 
 def load_districts():
-    with open('county_crosswalk.csv', 'rb') as csvfile:
-        reader = unicsv.UnicodeCSVDictReader(csvfile)
+    with open('county_crosswalk.csv', 'rU') as csvfile:
+        reader = csv.DictReader(csvfile)
         return [x for x in reader]
